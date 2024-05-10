559602390dc29fae50312de17baf88cf715a3c87
unknown <vismodeb2000@gmail.com>
2024-05-11 01:43:27 +0600

    Attractive offers secton add and property pypes some change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
-  // Nav Toggle
+  // Mobile Nav Toggle
   $('#toggleNav-icon').click(function(){
     $(this).toggleClass('open');
     $('.menu').slideToggle();
+  });
+
+  // Swiper Slider Property Types
+  var swiper = new Swiper(".propertyTypesItem", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerView: "auto",
+    rewind: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      991: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 60,
+      },
+    },
   });
   
 });
